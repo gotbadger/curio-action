d47052c7f6990f4a7efe499de2943836b35fd562
Philip Hayton <gotbadger@users.noreply.github.com>
2023-10-06 06:17:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Filter out any empty args
 args=$(for var in "$@"; do echo "$var";done | grep =.)
 ORIGIN_URL=$(git -C . remote get-url origin)
-RULE_BREACHES=`docker run --rm -e "SHA=$SHA" -e "ORIGIN_URL=$ORIGIN_URL" -e "CURRENT_BRANCH=$CURRENT_BRANCH" -e DIFF_BASE_BRANCH=$DIFF_BASE_BRANCH -e DEFAULT_BRANCH=$DEFAULT_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -v .:/tmp/scan bearer/bearer:canary-amd64 scan /tmp/scan --host=my.staging.bearer.sh --exit-code=0 --quiet ${args//$'\n'/ }`
+RULE_BREACHES=`docker run --rm -e SHA=$SHA -e "ORIGIN_URL=$ORIGIN_URL" -e "CURRENT_BRANCH=$CURRENT_BRANCH" -e DIFF_BASE_BRANCH=$DIFF_BASE_BRANCH -e DEFAULT_BRANCH=$DEFAULT_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -v .:/tmp/scan bearer/bearer:canary-amd64 scan /tmp/scan --host=my.staging.bearer.sh --exit-code=0 --quiet ${args//$'\n'/ }`
 SCAN_EXIT_CODE=$?
 
 echo "::debug::$RULE_BREACHES"
