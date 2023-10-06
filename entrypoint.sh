379a78bff3f349a375c370c9e9483e449a9a6047
Philip Hayton <gotbadger@users.noreply.github.com>
2023-10-06 04:28:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/Bearer/bearer/releases/download/untagged-57bcb7d3d621040187fe/bearer_1.26.0_linux_amd64.tar.gz
 tar -zxvf *.tar.gz bearer
 mv bearer $RUNNER_TEMP/
 
